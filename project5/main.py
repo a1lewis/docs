45db71dd77f52a858c6903fe7ec84fab46d12da3
a1lewis <72041512+a1lewis@users.noreply.github.com>
2021-01-11 11:49:28 -0500

    Update main.py (I made the change a few weeks ago, but forgot to upload it until now)

--- project5/main.py
+++ project5/main.py
@@ -5,8 +5,16 @@ import time  # control amount of time that commands work for
 from playsound import playsound
 import pyttsx3
 
+# Config
+ttsChoice = False  # Set to True if you would like the announcer to announce the game, this also enables/disables totals
+soundChoice = True  # Set to True if you would like to hear sounds
+customThreshold = 10  # Sets the number of points to win by in a compete game
+
 # Start TTS Engine
-engine = pyttsx3.init()
+if ttsChoice:
+    engine = pyttsx3.init()
+else:
+    print("Skipped TTS startup")
 
 # Set main variables
 run = 0
@@ -62,14 +70,18 @@ def green_win_flash():
 
 # Start game
 print("Starting game?")
-engine.say("Press both buttons to start a new game.")
-engine.runAndWait()
+if ttsChoice:
+    engine.say("Press both buttons to start a new game.")
+    engine.runAndWait()
+else:
+    print("Press both buttons to start a new game.")
 test = 1
 while test == 1:
     redButtonState = redButton.getState()
     greenButtonState = greenButton.getState()
     if redButtonState and greenButtonState:
-        playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
+        if soundChoice:
+            playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
         print("Starting game!")
         test = 0
 
@@ -85,8 +97,11 @@ while x < 5:
     x = x + 1
 while run == 0:
     # Select game mode
-    engine.say("Select a game mode. Press the red button for speed mode, or the green button for compete mode.")
-    engine.runAndWait()
+    if ttsChoice:
+        engine.say("Select a game mode. Press the red button for speed mode, or the green button for compete mode.")
+        engine.runAndWait()
+    else:
+        print("Select a game mode. Press the red button for speed mode, or the green button for compete mode.")
     totalButtonPresses = 0
     gameMode = 2
     # Game mode 0 = speed, Game mode 1 = compete, Game mode 2 = Setup
@@ -95,26 +110,37 @@ while run == 0:
         redButtonState = redButton.getState()
         if redButtonState and (not greenButtonState):
             totalButtonPresses = 1
-            playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
+            if soundChoice:
+                playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
             gameMode = 0
             redLED.setState(True)
             time.sleep(0.25)
             redLED.setState(False)
-            engine.say("Game mode selected: speed mode! First to ten points wins!")
-            engine.runAndWait()
+            if ttsChoice:
+                engine.say("Game mode selected: speed mode! First to ten points wins!")
+                engine.runAndWait()
+            else:
+                print("Game mode selected: speed mode! First to ten points wins!")
         if greenButtonState and (not redButtonState):
-            playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
+            if soundChoice:
+                playsound("/Users/Andrew/Downloads/level-pop-4-music-089278720_prev.mp3", False)
             totalButtonPresses = 1
             gameMode = 1
             greenLED.setState(True)
             time.sleep(0.25)
             greenLED.setState(False)
-            engine.say("Game mode selected: Compete mode! First to ten points more than the other wins!")
-            engine.runAndWait()
+            if ttsChoice:
+                engine.say("Game mode selected: Compete mode! First to ten points more than the other wins!")
+                engine.runAndWait()
+            else:
+                print("Game mode selected: Compete mode: First to ten points more than the other wins!")
 
     # Select players
-    engine.say("Player one, select your color.")
-    engine.runAndWait()
+    if ttsChoice:
+        engine.say("Player one, select your color.")
+        engine.runAndWait()
+    else:
+        print("Player one, select your color.")
     playerOne = 0
     redButtonPresses = 0
     greenButtonPresses = 0
@@ -132,23 +158,39 @@ while run == 0:
             greenLED.setState(True)
         time.sleep(0.02)
     if redButtonPresses == 1 and greenButtonPresses == 0:
-        engine.say("Player one has chosen red.")
-        engine.runAndWait()
+        if ttsChoice:
+            engine.say("Player one has chosen red.")
+            engine.runAndWait()
+        else:
+            print("Player one has chosen red.")
+            time.sleep(1)
         playerOne = 1
         redLED.setState(False)
         greenLED.setState(True)
-        engine.say("Player two, you have been assigned to green.")
-        engine.runAndWait()
+        if ttsChoice:
+            engine.say("Player two, you have been assigned to green.")
+            engine.runAndWait()
+        else:
+            print("Player two, you have been assigned to green.")
+            time.sleep(1)
         greenLED.setState(False)
         print("P1R P2G")
     elif greenButtonPresses == 1 and redButtonPresses == 0:
-        engine.say("Player one has chosen green.")
-        engine.runAndWait()
+        if ttsChoice:
+            engine.say("Player one has chosen green.")
+            engine.runAndWait()
+        else:
+            print("Player one has chosen green.")
+            time.sleep(1)
         playerOne = 2
         greenLED.setState(False)
         redLED.setState(True)
-        engine.say("Player two, you have been assigned to red.")
-        engine.runAndWait()
+        if ttsChoice:
+            engine.say("Player two, you have been assigned to red.")
+            engine.runAndWait()
+        else:
+            print("Player two, you have been assigned to red.")
+            time.sleep(1)
         redLED.setState(False)
         print("P1G P2R")
     else:
@@ -162,12 +204,17 @@ while run == 0:
     redScore = 0
     greenScore = 0
     winner = False
-    engine.say("Game will start when the lights come on, and will stop when you hear the following sound.")
-    engine.runAndWait()
-    playsound("/Users/Andrew/Downloads/341985__unadamlar__goodresult.wav", False)
-    time.sleep(5)
+    if ttsChoice:
+        engine.say("Game will start when the lights come on. "
+                   + "Game will stop when the lights stop reacting to your button presses.")
+        engine.runAndWait()
+    else:
+        print("Game will start when the lights come on. "
+              + "Game will stop when the lights stop reacting to your button presses.")
+    time.sleep(2)
 
     # Start game
+    print("Go!")
     redLED.setState(True)
     greenLED.setState(True)
     while not winner:
@@ -195,15 +242,16 @@ while run == 0:
                 winner = True
                 print("Green win with 10 points")
         if gameMode == 1:
-            if redScore >= greenScore + 10:
+            if redScore >= greenScore + customThreshold:
                 winner = True
                 print("Red win: " + str(redScore))
-            if greenScore >= redScore + 10:
+            if greenScore >= redScore + customThreshold:
                 winner = True
                 print("Green win: " + str(greenScore))
 
     # Play win sound
-    playsound("/Users/Andrew/Downloads/341985__unadamlar__goodresult.wav", False)
+    if soundChoice:
+        playsound("/Users/Andrew/Downloads/341985__unadamlar__goodresult.wav", False)
 
     # Flash Lights
     redLED.setState(False)
@@ -225,56 +273,60 @@ while run == 0:
         timesFlashed = timesFlashed + 1
 
     # Announce Winner
-    engine.say("The winner is...")
-    engine.runAndWait()
-    if gameMode == 1:
-        if redScore >= greenScore + 10:
-            if playerOne == 1:
-                red_win_flash()
-                engine.say("Player one, red, with " + str(redScore) + " points!")
-                engine.runAndWait()
-                playerOneGamesWon = playerOneGamesWon + 1
-            if playerOne == 2:
-                red_win_flash()
-                engine.say("Player two, red, with " + str(redScore) + " points!")
-                engine.runAndWait()
-                playerTwoGamesWon = playerTwoGamesWon + 1
-        if greenScore >= redScore + 10:
-            if playerOne == 2:
-                green_win_flash()
-                engine.say("Player one, green, with " + str(greenScore) + " points!")
-                engine.runAndWait()
-                playerOneGamesWon = playerOneGamesWon + 1
-            if playerOne == 1:
-                green_win_flash()
-                engine.say("Player two, green, with " + str(greenScore) + " points!")
-                playerTwoGamesWon = playerTwoGamesWon + 1
-    if gameMode == 0:
-        if redScore >= 10:
-            if playerOne == 1:
-                red_win_flash()
-                engine.say("Player one, red, with ten points!")
-                engine.runAndWait()
-                playerOneGamesWon = playerOneGamesWon + 1
-            if playerOne == 2:
-                red_win_flash()
-                engine.say("Player two, red, with ten points!")
-                engine.runAndWait()
-                playerTwoGamesWon = playerTwoGamesWon + 1
-        if greenScore >= 10:
-            if playerOne == 2:
-                green_win_flash()
-                engine.say("Player one, green, with ten points!")
-                engine.runAndWait()
-                playerOneGamesWon = playerOneGamesWon + 1
-            if playerOne == 1:
-                green_win_flash()
-                engine.say("Player two, green, with ten points!")
-                playerTwoGamesWon = playerTwoGamesWon + 1
+    if ttsChoice:
+        engine.say("The winner is...")
+        engine.runAndWait()
+        if gameMode == 1:
+            if redScore >= greenScore + 10:
+                if playerOne == 1:
+                    red_win_flash()
+                    engine.say("Player one, red, with " + str(redScore) + " points!")
+                    engine.runAndWait()
+                    playerOneGamesWon = playerOneGamesWon + 1
+                if playerOne == 2:
+                    red_win_flash()
+                    engine.say("Player two, red, with " + str(redScore) + " points!")
+                    engine.runAndWait()
+                    playerTwoGamesWon = playerTwoGamesWon + 1
+            if greenScore >= redScore + 10:
+                if playerOne == 2:
+                    green_win_flash()
+                    engine.say("Player one, green, with " + str(greenScore) + " points!")
+                    engine.runAndWait()
+                    playerOneGamesWon = playerOneGamesWon + 1
+                if playerOne == 1:
+                    green_win_flash()
+                    engine.say("Player two, green, with " + str(greenScore) + " points!")
+                    playerTwoGamesWon = playerTwoGamesWon + 1
+        if gameMode == 0:
+            if redScore >= 10:
+                if playerOne == 1:
+                    red_win_flash()
+                    engine.say("Player one, red, with ten points!")
+                    engine.runAndWait()
+                    playerOneGamesWon = playerOneGamesWon + 1
+                if playerOne == 2:
+                    red_win_flash()
+                    engine.say("Player two, red, with ten points!")
+                    engine.runAndWait()
+                    playerTwoGamesWon = playerTwoGamesWon + 1
+            if greenScore >= 10:
+                if playerOne == 2:
+                    green_win_flash()
+                    engine.say("Player one, green, with ten points!")
+                    engine.runAndWait()
+                    playerOneGamesWon = playerOneGamesWon + 1
+                if playerOne == 1:
+                    green_win_flash()
+                    engine.say("Player two, green, with ten points!")
+                    playerTwoGamesWon = playerTwoGamesWon + 1
 
     # New Game?
-    engine.say("Press the green button if you would like to start a new game. Otherwise, press the red button.")
-    engine.runAndWait()
+    if ttsChoice:
+        engine.say("Press the green button if you would like to start a new game. Otherwise, press the red button.")
+        engine.runAndWait()
+    else:
+        print("Press the green button if you would like to start a new game. Otherwise, press the red button.")
     awaitTimer = 0
     pressState = False
     while (awaitTimer < 60) and (not pressState):
@@ -295,10 +347,11 @@ while run == 0:
             break
 
 # Announce overall scores
-engine.say("Player one has won " + str(playerOneGamesWon) + " games!")
-engine.runAndWait()
-engine.say("Player two has won " + str(playerTwoGamesWon) + " games!")
-engine.runAndWait()
-engine.say("Thanks for playing!")
-engine.runAndWait()
+if ttsChoice:
+    engine.say("Player one has won " + str(playerOneGamesWon) + " games!")
+    engine.runAndWait()
+    engine.say("Player two has won " + str(playerTwoGamesWon) + " games!")
+    engine.runAndWait()
+    engine.say("Thanks for playing!")
+    engine.runAndWait()
 exit()
